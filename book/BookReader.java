206440904ef665af56047ff75b4481844e3c2364
Napat Srinamon <74853232+PuiFertilizer@users.noreply.github.com>
2022-03-18 21:51:07 +0700

    2

--- book/BookReader.java
+++ book/BookReader.java
@@ -4,8 +4,14 @@ package com.solid.book;
 import java.util.List;
 
 public class BookReader {
+    public void printToScreen() {
+        Book book = this.book;
+        do {
+            System.out.println(book.getCurrentPage());
+        } while (book.turnToNextPage());
+    }
     public static void main(String[] args) {
         Book book = new Book("Tyland", List.of("I", "moved", "here", "recently", "too"));
-        book.printToScreen();
+        printToScreen();
     }
 }
